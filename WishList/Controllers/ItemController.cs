a1deb4c0af8b02bdc8eef6f6effcf3048f246c64
Thodoris Digalakis <th.digalakis@gmail.com>
2020-03-28 22:19:11 +0200

    Complete itemController

--- WishList/Controllers/ItemController.cs
+++ WishList/Controllers/ItemController.cs
@@ -39,7 +39,7 @@ namespace WishList.Controllers
 
         public IActionResult Delete(int Id)
         {
-            var item = _context.Items.FirstOrDefault(i => i.Id == Id);
+            var item = _context.Items.FirstOrDefault(e => e.Id == Id);
             _context.Items.Remove(item);
             _context.SaveChanges();
             return RedirectToAction("Index");
